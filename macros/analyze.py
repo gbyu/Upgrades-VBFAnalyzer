00ea23d171e6a2aa4159a285d4ca905ce6b290c7
lata <panwarlsweet@gmail.com>
2018-10-28 17:05:27 +0100

    Adding selections before Mqq for FullSim

--- macros/analyze.py
+++ macros/analyze.py
@@ -231,8 +231,8 @@ def main():
   h_cutflow                    = ROOT.TH1D("h_cutflow"                    ,";;Events;"                     ,10   ,0.5    ,10.5 )
       
   h_cutflow.GetXaxis().SetBinLabel(1 ,"All evts") ; 
-#  h_cutflow.GetXaxis().SetBinLabel(2 ,"AK8 jets") ; 
-  h_cutflow.GetXaxis().SetBinLabel(2 ,"M_{qq}") ;
+  h_cutflow.GetXaxis().SetBinLabel(2 ,"AK8 jets") ; 
+#  h_cutflow.GetXaxis().SetBinLabel(2 ,"M_{qq}") ;
   h_cutflow.GetXaxis().SetBinLabel(3 ,"p_{T}+#eta") ; 
   h_cutflow.GetXaxis().SetBinLabel(4 ,"#Delta#Eta(JJ)") ; 
   h_cutflow.GetXaxis().SetBinLabel(5 ,"#tau_{21}") ; 
@@ -274,8 +274,35 @@ def main():
       gen_id = event.GenParticles_genpid
       gen_status = event.GenParticles_genstatus
       gen_momstatus = event.GenParticles_mom0status
-  
+      pts = event.AK8JetsPuppi_pt
+      nak8 = len(pts)
       h_cutflow.Fill(1)
+      
+      ### Selecting at least two AK8 jets:	
+      if nak8 < 2: continue	
+      etas   = event.AK8JetsPuppi_eta	
+      ptsel  = pts[0] > opt.ptak8_0_Min and pts[1] > opt.ptak8_1_Min	
+      etasel = abs(etas[0]) < opt.etaak8_0_Max and abs(etas[1]) < opt.etaak8_1_Max	
+      detasel = abs(etas[0] - etas[1]) < opt.detaak8Max	
+      tau1s           = event.AK8JetsPuppi_tau1Puppi	
+      tau2s           = event.AK8JetsPuppi_tau2Puppi	
+  	
+      tau21sel = tau2s[0]/tau1s[0] < opt.t21_0_Max and tau2s[1]/tau1s[1] < opt.t21_1_Max	
+  	
+      sd_masses       = event.AK8JetsPuppi_softDropMassPuppi 	
+   	
+      msdsel     = opt.msd_0_Min < sd_masses[0] < opt.msd_0_Max and opt.msd_1_Min < sd_masses[1] < opt.msd_1_Max	
+      #### Store Higgs cand p4 for future use	
+      phis            = event.AK8JetsPuppi_phi	
+      masses          = event.AK8JetsPuppi_mass	
+  	
+      p4_higgses= []	
+      p4_ak80 = ROOT.TLorentzVector()	
+      p4_ak81 = ROOT.TLorentzVector()	
+      p4_ak80.SetPtEtaPhiM(pts[0], etas[0], phis[0], masses[0])	
+      p4_ak81.SetPtEtaPhiM(pts[1], etas[1], phis[1], masses[1])	
+      p4_higgses.append(p4_ak80)	
+      p4_higgses.append(p4_ak81)
 
 #### adding partonic mass distribution #####
       p4_p1 = ROOT.TLorentzVector()
@@ -289,8 +316,9 @@ def main():
                 countqq = 0
       		p4_p1.SetPtEtaPhiM(gen_pt[count1], gen_eta[count1], gen_phi[count1], gen_mass[count1])
                 p4_p2.SetPtEtaPhiM(gen_pt[count2], gen_eta[count2], gen_phi[count2], gen_mass[count2])
-                h_cutflow.Fill(2)
+
                 h_mass_qq.Fill((p4_p1+p4_p2).M())
+
                 break; 
 
   fout.cd()
